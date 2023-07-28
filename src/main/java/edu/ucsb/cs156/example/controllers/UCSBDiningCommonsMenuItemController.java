e1e43559f64f3fe3dfe71072395a4145bdd85396
Matt Priston <mpristoncarruthersfi@ucsb.edu>
2023-07-28 16:54:49 -0700

    added controller

--- src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/UCSBDiningCommonsMenuItemController.java
@@ -1,6 +1,6 @@
 package edu.ucsb.cs156.example.controllers;
 
-import edu.ucsb.cs156.example.entities.UCSBDiningCommonsMenuItem;
+import edu.ucsb.cs156.example.entities.MenuItemReview;
 import edu.ucsb.cs156.example.errors.EntityNotFoundException;
 import edu.ucsb.cs156.example.repositories.UCSBDiningCommonsMenuItemRepository;
 
@@ -32,5 +32,87 @@ import javax.validation.Valid;
 @RestController
 @Slf4j
 public class UCSBDiningCommonsMenuItemController extends ApiController{
+
+    @Autowired
+    UCSBDiningCommonsMenuItemRepository ucsbDiningCommonsMenuItemRepository;
+
+    @Operation(summary= "List all UCSB Dining Commons Menu Items Reviews")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<MenuItemReview> allUCSBDiningCommonsMenuItems() {
+        Iterable<MenuItemReview> menuItemReview = ucsbDiningCommonsMenuItemRepository.findAll();
+        return menuItemReview;
+    }
+
+    @Operation(summary= "Get a single UCSB Dining Commons Menu Item Review")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public MenuItemReview getById(
+            @Parameter(name="id") @RequestParam Long id) {
+        MenuItemReview menuItemReview = ucsbDiningCommonsMenuItemRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+
+        return menuItemReview;
+    }
+
+    @Operation(summary= "Create a new UCSB Dining Commons Menu Item Review")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public MenuItemReview postMenuItemReview(
+        @Parameter(name="itemId") @RequestParam long itemId,
+        @Parameter(name="reviewerEmail") @RequestParam String reviewerEmail,
+        @Parameter(name="stars") @RequestParam int stars,
+        @Parameter(name="dateReviewed") @RequestParam("dateAdded") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateReviewed,
+        @Parameter(name="comments") @RequestParam String comments) 
+        throws JsonProcessingException {
+
+            MenuItemReview menuItemReview = new MenuItemReview();
+
+            menuItemReview.setItemId(itemId);
+            menuItemReview.setReviewerEmail(reviewerEmail);
+            menuItemReview.setStars(stars);
+            menuItemReview.setDateReviewed(dateReviewed);
+            menuItemReview.setComments(comments);
+
+            MenuItemReview savedItem = ucsbDiningCommonsMenuItemRepository.save(menuItemReview);
+
+            return savedItem;
+        }
     
+    @Operation(summary= "Update a UCSB Dining Commons Menu Item Review")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("")
+    public MenuItemReview updateMenuItemReview(
+        @Parameter(name="id") @RequestParam Long id,
+        @RequestBody @Valid MenuItemReview incoming){
+            MenuItemReview menuItemReview = ucsbDiningCommonsMenuItemRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+
+            menuItemReview.setItemId(incoming.getItemId());
+            menuItemReview.setReviewerEmail(incoming.getReviewerEmail());
+            menuItemReview.setStars(incoming.getStars());
+            menuItemReview.setDateReviewed(incoming.getDateReviewed());
+            menuItemReview.setComments(incoming.getComments());
+
+            ucsbDiningCommonsMenuItemRepository.save(menuItemReview);
+            
+
+            return menuItemReview;
+        }
+
+    @Operation(summary= "Delete a UCSB Dining Commons Menu Item Review")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping("")
+    public Object deleteMenuItemReview(
+        @Parameter(name="id") @RequestParam Long id){
+        
+        MenuItemReview menuItemReview = ucsbDiningCommonsMenuItemRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(MenuItemReview.class, id));
+
+        ucsbDiningCommonsMenuItemRepository.delete(menuItemReview);
+        return genericMessage("MenuItemReview with id $s deleted".formatted(id));
+        }
+
 }
+
+
